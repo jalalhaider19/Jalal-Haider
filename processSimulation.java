b17518c42c4df8265fe9c9672ecd2ab273835ae5
jalalhaider19 <166186240+jalalhaider19@users.noreply.github.com>
2024-06-24 12:19:36 -0400

    Update and rename PartB.java to processSimulation.java

--- processSimulation.java
+++ processSimulation.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-public class PartB {
+public class processSimulation {
     public static void main(String[] args) {
         List<Integer> processes = new LinkedList<>(); //creates a list of processes in a chain 
         Random random = new Random(); //random instantiated from Random
